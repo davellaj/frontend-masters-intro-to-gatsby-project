de0ac77d3703ed5b1a0066e39075b58cb8a642cd
Jamie Davella <jamie.davella@gmail.com>
2022-02-13 23:54:02 -0600

    add Seo and Layout components to use on index and about pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,16 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
+import Layout from '../components/layout.js'
 
 const AboutPage = () => {
     return (
-        <main>
+        <Layout
+            title="about this site"
+            description="more about this site"
+        >
             <h1>About Page</h1>
             <Link to="/">Go back to home</Link>
-        </main>
+        </Layout>
     )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,13 @@
 import * as React from 'react'
-import { Link, useStaticQuery, graphql } from 'gatsby'
+import { Link } from 'gatsby'
+import Layout from '../components/layout.js'
 
 const IndexPage = () => {
-    const data = useStaticQuery(graphql`
-        query getSiteTitle {
-            site {
-                siteMetadata {
-                    title
-                }
-            }
-        }
-    `)
-
-    const meta = data?.site?.siteMetadata ?? {}
-
     return (
-        <>
-            <header>
-                <Link to="/">{meta.title}</Link>
-            </header>
-            <main>
-                <h1>Frontend Masters Gatsby Site</h1>
-                <Link to="/about">About this site</Link>
-            </main>
-        </>
+        <Layout>
+            <h1>Frontend Masters Gatsby Site</h1>
+            <Link to="/about">About this site</Link>
+        </Layout>
     )
 }
 
