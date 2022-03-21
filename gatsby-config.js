4d1ea76f51ac8005cc4426bdcd67f996b3501a7d
Jamie Davella <jamie.davella@gmail.com>
2022-03-21 08:11:48 -0600

    add mdx support

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,37 @@ module.exports = {
         "https://res.cloudinary.com/jlengstorf/image/upload/v1628127675/frontend-masters/gatsby-intro/share-image.jpg",
     },
     plugins: [
-        'gatsby-plugin-react-helmet'
-    ],
+      'gatsby-plugin-react-helmet',
+     {
+       resolve: 'gatsby-source-filesystem',
+       options: {
+         name: 'posts',
+         path: `${__dirname}/src/posts/`,
+       },
+     },
+     {
+       resolve: 'gatsby-plugin-page-creator',
+       options: {
+         path: `${__dirname}/src/posts/`,
+       },
+     },
+     {
+       resolve: 'gatsby-plugin-mdx',
+       options: {
+         defaultLayouts: {
+           posts: require.resolve('./src/components/post-layout.js'),
+         },
+       },
+     },
+     {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: `${__dirname}/src/images`,
+      },
+    },
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    ]
   };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,37 @@
 import * as React from 'react'
-import { Link } from 'gatsby'
+import { graphql, Link, useStaticQuery } from 'gatsby'
 import Layout from '../components/layout.js'
 
 const IndexPage = () => {
+    const data = useStaticQuery(graphql`
+        query GetBlogPosts {
+            allMdx(sort: {fields: frontmatter___date, order: DESC}) {
+                nodes {
+                    frontmatter {
+                        description
+                        title
+                        date(fromNow: false)
+                    }
+                    id
+                    slug
+                }
+            }
+        }
+    `)
+     const posts = data.allMdx.nodes;
     return (
         <Layout>
             <h1>Frontend Masters Gatsby Site</h1>
             <Link to="/about">About this site</Link>
+            <h2>Check out my other blog posts</h2>
+            <ul>
+                {posts.map(post => (
+                     <li key={post.frontmatter.id}>
+                        <Link to={post.slug}>{post.frontmatter.title}</Link>{' '}
+                        <small>posted: {post.frontmatter.date}</small>
+                    </li>
+                ))}
+            </ul>
         </Layout>
     )
 }
